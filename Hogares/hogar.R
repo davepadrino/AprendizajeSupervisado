6864ff31b45f18b4b75884920d44ab1f0ea2234f
davepadrino <david.padrino3@hotmail.com>
2016-03-10 08:33:59 -0430

    2nd commit

--- Hogares/hogar.R
+++ Hogares/hogar.R
@@ -1,8 +1,6 @@
 # install.packages("readxl") ----
-# https://maps.googleapis.com/maps/api/distancematrix/json?origins=Via+Paolo+Emilio|Vancouver+BC|Seattle&destinations=Piazzale+Aldo+Moro|San+Francisco|Victoria+BC&key=AIzaSyDfuJjIpDfqug_x8A1p8eo6S0Z13L8XfrE
-# https://maps.googleapis.com/maps/api/distancematrix/json?origins=Via+Paolo+Emilio|Vancouver+BC|Seattle&destinations=Sapienza+Universidad+de+Roma&key=AIzaSyDfuJjIpDfqug_x8A1p8eo6S0Z13L8XfrE
 library("readxl")
-
+library(stringr) 
 # Preparing data ----
 tablas = read_excel("hogares.xlsx")
 data.hogar = as.data.frame(tablas)
@@ -11,13 +9,6 @@ data.hogar$Foto <- NULL
 data.hogar$Dirección <- (gsub("(\n)+", " ", data.hogar$Dirección))
 data.hoga$`Tipo de Inmueble` <- gsub("(Mini.)?(A|a)p.+", "1", data.hoga$`Tipo de Inmueble`) # putting (mini)?apartments as 0
 data.hoga$`Tipo de Inmueble` <- gsub("Mo.+", "0", data.hoga$`Tipo de Inmueble`) # putting monolocale as 0
-
-
-
-
-# Via+San+Roberto+Bellarimino
-# Vía Monte Verde, Boynton Beach, FL 33436, EE. UU."
-# "Condado de Gallia, Ohio, EE. UU." 
 
 
 
--- Hogares/tester_file.R
+++ Hogares/tester_file.R
@@ -8,10 +8,10 @@ test.data <- read.csv("hogares.csv")
 # test.data <- na.omit(test.data)
 # test.data$Distrito <- (gsub("(\n)+", " ", test.data$Distrito))
 # test.data$Dirección <- (gsub("(\n)+", " ", test.data$Dirección))
-#test.data$Dirección[33] <- "Via San Roberto Bellarmino"
-#test.data$Foto <- NULL
-#test.data$`Tipo de Inmueble` <- gsub("(Mini.)?(A|a)p.+", "1", test.data$`Tipo de Inmueble`) # putting (mini)?apartments as 0
-#test.data$`Tipo de Inmueble` <- gsub("Mo.+", "0", test.data$`Tipo de Inmueble`) # putting monolocale as 0
+# test.data$Dirección[33] <- "Via San Roberto Bellarmino"
+# test.data$Foto <- NULL
+# test.data$`Tipo de Inmueble` <- gsub("(Mini.)?(A|a)p.+", "1", test.data$`Tipo de Inmueble`) # putting (mini)?apartments as 0
+# test.data$`Tipo de Inmueble` <- gsub("Mo.+", "0", test.data$`Tipo de Inmueble`) # putting monolocale as 0
 # location <- character()
 # location.dir.it <- character()
 
@@ -22,73 +22,74 @@ test.data <- read.csv("hogares.csv")
 # test.data["location"] <- location
 
 
-test.data$Piso # Not yet, 'til answered doub
+# To filter only sigle rooms
+doppia <- grep("(dop)+.", test.data$Habitaciones.Disponibles) # works
+posto <- grep("(posto)+.", test.data$Habitaciones.Disponibles) #works
+test <- test.data
+test <- test[-doppia,] #works
+test <- test[-posto,] #works
 
+# Take smaller value of the arrays ----
+prices <- gsub("\\D", "", test$Precio.Mensual)
+prices <- as.numeric(prices)
+precios <- integer()
 
-class(test$`Precio Mensual`)
-as.factor(test.data$Precio.Mensual)
-
-
-class(test.data$Precio.Mensual) 
-
-
-# Test department ----
-
-## descripcion
-  test.data$Descripción
-  des <- test.data$Descripción
-  des <- as.factor(des)
-  levels(des)
-  # camera (singular)/camere (plural)
-  # bango (singular)/ bagni (plural)
-
-  grep("ingr+", levels(des))
-
-## habitaciones
-  test.data$`Habitaciones Disponibles`
-  hab <- test.data$`Habitaciones Disponibles`
-  hab <- as.factor(hab)
-  levels(hab)
-  index <- grep("singo+", hab)
-  index[2]  
-  hab[index[3]]
-
-## montly price
-  test.data$`Precio Mensual`
-  price <- test.data$`Precio Mensual`
-  price <- as.factor(price)    
-  levels(price)
+for(i in 1:length(prices)){
+  if (str_length(prices[i]) > 4){
+    len <- str_length(prices[i])
+    full.array <- prices[i]
+    min.value <- integer()
+    while(len >= 3){
+      number.of.parts <- 3
+      len <- len - number.of.parts
+      current.val <- full.array%/%(1*10^len)
+      min.value <- c(min.value, current.val)
+      full.array <- full.array%%(1*10^len)
+    }
+    precios[i] <- min(min.value)
+  }else{
+    precios[i] <- prices[i]
+  }
   
-  ### THIS ONE GETS JUST 1 PRICE ###
-  integer.prices <- as.numeric(gsub("€", "", gsub("([0-9]+).*$", "\\1", price))) # get numbers from RE
-  ##################################
-  
-   object.in.room <- gsub("€|\n|;| ", "", (gsub("[0-9]+", "\\1", price))) # GET elements in the room
+}
 
-  
-  
-  
+test['precios.numeros'] <- precios
 
-## notes  
-  test.data$Notas
-  notes <- test.data$Notas
-  # test every field have a "ragazzi/e ----
-    notes2 <- test.data$Notas
-    notes2 <- gsub("ragazzi", "######", notes2) 
-    notes2 <- gsub("ragazze", "######", notes2)
-    grep("ragazzi", notes)
-    grep("ragazze", notes)
-  
-  
-  
-  
-# Already tested and implemented in 'test.data'
-inmueble <- test.data$`Tipo de Inmueble`
-class(inmueble)
-inmueble <-as.factor(inmueble)
-inmueble <- gsub("(Mini.)?(A|a)p.+", "1", inmueble) # putting (mini)?apartments as 0
-inmueble <- gsub("Mo.+", "0", inmueble) # putting apartments as 0
+# separate boys & girls ----
+gender <- character()
+test.data$Notas
+notes <- test.data$Notas
+# test every field have a "ragazzi/e ----
+grep("ragazzi", notes)
+grep("ragazze", notes)
+mix <- grep("[ra.]/[ra.]", notes)
+index.boys <- setdiff(grep("ragazzi", notes), grep("[a-z]/[a-z]", notes))
+index.girls <- setdiff(grep("ragazze", notes), grep("[a-z]/[a-z]", notes))
 
+
+grep(".[ra]+. / [ra]+.", notes)
+
+
+
+
+
+
+
+
+
+
+
+# extract number of features to a new column
+
+calificacion.inmueble <- integer()
+for (i in 1:nrow(test)){
+  calificacion.inmueble[i] <- str_count(test$Descripción[i], ',') + str_count(test$Descripción[i], ' e ') + 1
+  if (any(grep('(tut)+', test$Precio.Mensual[i], ignore.case = T))){
+    calificacion.inmueble[i] <- calificacion.inmueble[i] +5
+  }else{
+    calificacion.inmueble[i] <- calificacion.inmueble[i] + str_count(test$Precio.Mensual[i], ',') + str_count(test$Precio.Mensual[i], ' e ') + 1
+  }
+}  
 
 
 
@@ -102,36 +103,22 @@ sub('.*-([0-9]+).*','\\1',x)
 
 
 
-prices <- gsub("\\D", " ", test.data$Precio.Mensual) # works!
-doppia <- grep("(dop)+.", test.data$Habitaciones.Disponibles) # works
-posto <- grep("(posto)+.", test.data$Habitaciones.Disponibles) #works
-test <- test.data
-test <- test[-doppia,] #works
-test <- test[-posto,] #works
-prices2 <- gsub("\\D", " ", test$Precio.Mensual)
-
-library(stringr) #
-calificacion.inmueble <- integer()
-str_count(test$Descripción[1], ',') #
-str_count(test.data$Descripción[19], ',') #
-any(grep('(TUT|Tut|tut)+', test.data$Precio.Mensual[8])) #
-
-
-
-for (i in 1:nrow(test.data)){
- calificacion.inmueble[i] <- str_count(test.data$Descripción[i], ',') + str_count(test.data$Descripción[i], ' e ') + 1
- if (any(grep('(TUT|Tut|tut)+', test.data$Precio.Mensual[i]))){
-   calificacion.inmueble[i] <- calificacion.inmueble[i] +5
- }else{
-   calificacion.inmueble[i] <- calificacion.inmueble[i] + str_count(test.data$Precio.Mensual[i], ',') + str_count(test.data$Precio.Mensual[i], ' e ') + 1
- }
-}  
 
 
 
 
 
-grep('(tutt)+', test.data$Precio.Mensual[2])
+str_count(test$Descripción[1], ',') #works
+str_count(test.data$Descripción[19], ',') #works
+any(grep('(tut)+', test.data$Precio.Mensual[8]), ignore.case = T) #works
+
+
+
+
+
+
+
+
 
 
 ###########################################
